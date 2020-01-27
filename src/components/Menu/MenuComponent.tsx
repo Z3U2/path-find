25ea39e717b96abf5b444d5be25fdc3002248afc
z3u2 <ilyassnasr@gmail.com>
2020-01-27 20:16:27 +0100

    Add Menu and Grid Components

---
+++ src/components/Menu/MenuComponent.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+
+export class MenuComponent extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>
+          Hey
+      </div>
+    );
+  }
+}
