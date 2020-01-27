30c60d84e5f4c666bb534ea0d57b84eb740eb696
z3u2 <ilyassnasr@gmail.com>
2020-01-27 20:14:37 +0100

    Add root component and Start component

---
+++ src/components/Start/StartComponent.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+
+import { LayoutComponent } from "../Layout/LayoutComponent";
+import { CreatorComponent } from "../Creator/CreatorComponent"
+import Grid from "../../domain/Grid"
+
+export interface IGrid {
+    grid: Grid;
+    setGrid(grid: Grid): void;
+}
+
+export class StartComponent extends React.Component<{}, {grid:Grid}> {
+  
+    constructor(props:{}) {
+        super(props);
+        this.state= {
+            grid: null
+        }
+        this.setGrid = this.setGrid.bind(this)
+    }
+
+    setGrid(grid: Grid) {
+        this.setState({
+            grid: grid
+        })
+    }
+
+  render() {
+    return (
+      <div>
+        {!this.state.grid && (
+          <CreatorComponent grid={this.state.grid} setGrid={this.setGrid} />
+        )}
+        {this.state.grid && (
+          <LayoutComponent grid={this.state.grid} setGrid={this.setGrid} />
+        )}
+      </div>
+    );
+  }
+}
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./style.css";
+
+import {StartComponent} from "./components/Start/StartComponent";
+
+ReactDOM.render(
+  <StartComponent />,
+  document.getElementById("path-app")
+);
