1e08ac7e84b0704c7cf06d8c84d14ae88bac8e5c
z3u2 <ilyassnasr@gmail.com>
2020-01-27 23:20:19 +0100

    Add Grid Service

--- src/services/GridService.ts
+++ src/services/GridService.ts
@@ -1,16 +1,31 @@
 import Grid from "../domain/Grid";
+import ISquare from "../domain/ISquare";
+import NoDetailSquare from "../domain/NoDetailSquare";
 
 export interface GridService {
-    getGrid(): Grid;
+    getGrids(): Grid[];
     saveGrid(grid: Grid): void;
 }
 
-export class HardCodeGridService {
+export class HardCodeGridService implements GridService{
 
     grid : Grid;
 
     constructor() {
-        this.grid = new Grid(10,10);
-        
+        this.grid = new Grid(10,10,"a grid");
+        let square1: ISquare = new NoDetailSquare(5, 5);
+        let square2: ISquare = new NoDetailSquare(5, 8);
+        this.grid.add(square1);
+        this.grid.add(square2);
+        this.getGrids = this.getGrids.bind(this);
+        this.saveGrid = this.saveGrid.bind(this);
+    }
+
+    saveGrid(grid: Grid):void {
+        return;
+    }
+
+    getGrids() :Grid[] {
+        return [this.grid];
     }
 }
