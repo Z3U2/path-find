79578f4013c30dd44f253eeefcce420896a7d91a
z3u2 <ilyassnasr@gmail.com>
2020-01-27 02:12:59 +0100

    Add ts build pipeline

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  mode: "production",
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' as resolvable extensions.
+    extensions: [".ts"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+};
