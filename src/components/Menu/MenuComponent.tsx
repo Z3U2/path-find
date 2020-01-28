981802495507fc10070c61ae74f9c66ec3052d67
z3u2 <ilyassnasr@gmail.com>
2020-01-28 16:32:29 +0100

    Add info to MenuComponent

--- src/components/Menu/MenuComponent.tsx
+++ src/components/Menu/MenuComponent.tsx
@@ -1,15 +1,33 @@
 import * as React from "react";
+import "./style.css"
 
 
 import DraggableSquare from "./DraggableSquare";
 import ItemTypes from "./ItemTypes";
-export class MenuComponent extends React.Component<{}, {}> {
+import { IGridState } from "../Start/StartComponent";
+
+export class MenuComponent extends React.Component<IGridState, {}> {
   render() {
+      let name = this.props.grid.name;
+      let height = this.props.grid.height;
+      let width = this.props.grid.width;
+      name = name.split(" ").join("-");
     return (
       <div className="menu-wrapper">
+        <div className="menu-info-wrapper">
+          {`path://${name}/${height}x${width}`}
+        </div>
         <div className="drag-me">
           <p>Drag me :</p>
           <DraggableSquare type={ItemTypes.SQUARE} />
+        </div>
+        <div className="left-click">
+          <p>Left Click two </p>
+          <div
+            className="draggable-square"
+            style={{cursor:'auto'}}
+          ></div>
+          <p> to find shortest path</p>
         </div>
       </div>
     );
