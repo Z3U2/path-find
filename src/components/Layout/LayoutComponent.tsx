07dce9fb1ea9ad6b62da398fe9a29e97f0bfe08b
z3u2 <ilyassnasr@gmail.com>
2020-01-28 16:35:57 +0100

    Add Drag and Drop provider and Menu props

--- src/components/Layout/LayoutComponent.tsx
+++ src/components/Layout/LayoutComponent.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
+import "./style.css"
+import {DndProvider} from "react-dnd";
+import Backend from "react-dnd-html5-backend";
 
 import { GridComponent } from "../Grid/GridComponent";
-import Grid from "../../domain/Grid"
 import { IGridState } from "../Start/StartComponent";
 import { MenuComponent } from "../Menu/MenuComponent";
 
@@ -10,8 +12,10 @@ export class LayoutComponent extends React.Component<IGridState, {}> {
   render() {
     return (
       <div className="grid-wrapper">
-        <MenuComponent />
-        <GridComponent {...this.props} />
+        <DndProvider backend={Backend}>
+          <MenuComponent {...this.props} />
+          <GridComponent {...this.props} />
+        </DndProvider>
       </div>
     );
   }
