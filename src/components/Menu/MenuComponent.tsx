ec5cdba68f7d92d17c1c978e93c5d02f89d0a4ff
z3u2 <ilyassnasr@gmail.com>
2020-01-28 16:31:30 +0100

    Add DraggableSquare to Menu

--- src/components/Menu/MenuComponent.tsx
+++ src/components/Menu/MenuComponent.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
 
 
+import DraggableSquare from "./DraggableSquare";
+import ItemTypes from "./ItemTypes";
 export class MenuComponent extends React.Component<{}, {}> {
   render() {
     return (
-      <div>
-          Hey
+      <div className="menu-wrapper">
+        <div className="drag-me">
+          <p>Drag me :</p>
+          <DraggableSquare type={ItemTypes.SQUARE} />
+        </div>
       </div>
     );
   }
